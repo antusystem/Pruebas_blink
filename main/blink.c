25648411092a8312019e1c011b2bce2274af13a0
antusystem <aleantunes95@gmail.com>
2020-05-18 14:34:56 -0400

    No se que tiene en este punto

--- main/blink.c
+++ main/blink.c
@@ -12,11 +12,15 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "freertos/queue.h"
+#include "driver/rtc_io.h"
+#include "esp_sleep.h"
+
 
 /* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
 #define BLINK_GPIO CONFIG_BLINK_GPIO
+#define ext_wakeup_pin GPIO_NUM_26
 
 static EventGroupHandle_t event_group;
 
@@ -38,12 +42,15 @@ const int SYNC_BIT_TASK2 = BIT5;
 
 
 QueueHandle_t xQueue;
+QueueHandle_t xQueue2;
 
 
 int a = 0;
 int b = 0;
 int *b1 ;
 int c = 0;
+esp_err_t err;
+esp_sleep_wakeup_cause_t causa;
 
 
 uint8_t puerta_abierta = 0;
@@ -75,11 +82,11 @@ void Retraso2 (void *P){
 		xEventGroupWaitBits(event_group,BEGIN_TASK2,true,true,portMAX_DELAY);
 		printf("Esperare 5 s \r\n");
 		vTaskDelay(5000 / portTICK_PERIOD_MS);
-		xQueueReceive(xQueue,&c,portMAX_DELAY);
+		xQueuePeek(xQueue,&c,portMAX_DELAY);
 		printf("c es %d\r\n",c);
 		b++;
 		printf("Esperare 5.1 \r\n");
-		xQueueSendToFront(xQueue,&b,portMAX_DELAY);
+		xQueueSendToFront(xQueue2,&b,portMAX_DELAY);
 		printf("Ya espere 5.2 \r\n");
 		printf("Ya espere 5.3 \r\n");
 		xEventGroupSetBits(event_group, SYNC_BIT_TASK2);
@@ -93,14 +100,14 @@ void Retraso3 (void *P){
 	for(;;){
 		//Groupsyn setea el bit del segundo parametro y luego espera el tiempo desado por los bits que se
 		//seleccionen
-		xEventGroupSync(event_group,BEGIN_TASK1,0x30,portMAX_DELAY);
-	//	xEventGroupWaitBits(event_group,BEGIN_TASK3,true,true,portMAX_DELAY);
-		xEventGroupClearBits(event_group, BEGIN_TASK2);
+		xEventGroupSync(event_group,BEGIN_TASK4,0x30,portMAX_DELAY);
 		xEventGroupClearBits(event_group, SYNC_BIT_TASK2);
 		xEventGroupClearBits(event_group, SYNC_BIT_TASK1);
 		printf("Esperare 6 s \r\n");
 		vTaskDelay(6000 / portTICK_PERIOD_MS);
 		xQueueReceive(xQueue,&c,portMAX_DELAY);
+		printf("c es %d \r\n",c);
+		xQueueReceive(xQueue2,&c,portMAX_DELAY);
 		printf("c es %d \r\n",c);
 		printf("Ya espere 6.1 \r\n");
 		b++;
@@ -136,51 +143,48 @@ void app_main(void)
        Technical Reference for a list of pads and their default
        functions.)
     */
+    uint8_t * ronda =  malloc(sizeof( uint8_t ));
+    uint8_t b[2] = {0};
+    *ronda = 0;
+    printf("ronda es %d \r\n",*ronda );
+    b[*ronda] = 1;
+	printf("b es %d \r\n",b[*ronda] );
+    *ronda = *ronda +1;
 
-	a = 2;
+	a = 4;
+    b[*ronda] = 5 ;
+    causa = esp_sleep_get_wakeup_cause();
+
+	printf("la causa es %d \r\n",a );
 
 	printf("%d \r\n",a );
+	printf("b es %d \r\n",b[*ronda] );
+	printf("ronda es %d \r\n",*ronda );
+	rtc_gpio_init(ext_wakeup_pin);
+	rtc_gpio_set_direction(ext_wakeup_pin, RTC_GPIO_MODE_INPUT_ONLY);
+
+	err = rtc_gpio_wakeup_enable(ext_wakeup_pin,  GPIO_INTR_HIGH_LEVEL);
+	err = esp_sleep_enable_ext0_wakeup(ext_wakeup_pin, 1);
+	printf("err es %d \r\n", err);
+	vTaskDelay(1000 / portTICK_PERIOD_MS);
+	esp_deep_sleep_start();
+
 
 
 	xQueue = xQueueCreate( 5, sizeof( int32_t ) );
+	xQueue2 = xQueueCreate( 5, sizeof( int32_t ) );
 	event_group = xEventGroupCreate();
-
-	if (xEventGroupGetBits(event_group) == 0x10){
-		printf("El grupo es 0x30 \r\n");
-	}
 
 
 	xTaskCreatePinnedToCore(&Retraso1, "Retraso1", 1024*2, NULL, 8, NULL,0);
 	xTaskCreatePinnedToCore(&Retraso2, "Retraso2", 1024*2, NULL, 6, NULL,0);
 	xTaskCreatePinnedToCore(&Retraso3, "Retraso3", 1024*2, NULL, 4, NULL,0);
 
-	if (xEventGroupGetBits(event_group) == 0x10){
-		printf("El grupo es 0x30 \r\n");
-	}
-	xTaskCreatePinnedToCore(&Retraso4, "Retraso4", 1024, NULL, 10, NULL,0);
-
-	if (xEventGroupGetBits(event_group) == 0x10){
-		printf("El grupo es 0x30 \r\n");
-	}
-
-	if (xEventGroupGetBits(event_group) == 0x00){
-		printf("El grupo es 0x00 \r\n");
-	}
-
-	if (xEventGroupGetBits(event_group) == 0x10){
-		printf("El grupo es 0x30 \r\n");
-	}
 
 	printf("%d \r\n",a );
 //	xEventGroupSetBits(event_group, BEGIN_TASK1);
 
-	if (xEventGroupGetBits(event_group) == 0x30){
-		printf("El grupo es 0x30 \r\n");
 
-	}
-	if (xEventGroupGetBits(event_group) == 0x10){
-		printf("El grupo es 0x30 \r\n");
-	}
 
 
 
