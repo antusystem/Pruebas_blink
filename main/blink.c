962da326d5a5ffd78e6730f033be4873aa8b925b
antusystem <aleantunes95@gmail.com>
2020-05-20 11:32:42 -0400

    Esta lo basico para probar

--- main/blink.c
+++ main/blink.c
@@ -1,149 +1,209 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+/*********************************************************************************
+*********************************PRUEBA BUFFER UART********************************
+ ********************************* PROYECTO SENIAT ********************************
+***********************************************************************************/
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/uart.h"
+#include "esp_log.h"
 #include "driver/gpio.h"
 
-/* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+static const char *TAG = "uart_events";
 
-static EventGroupHandle_t event_group;
+/**
+ * This example shows how to use the UART driver to handle special UART events.
+ *
+ * It also reads data from UART0 directly, and echoes it to console.
+ *
+ * - Port: UART0
+ * - Receive (Rx) buffer: on
+ * - Transmit (Tx) buffer: off
+ * - Flow control: off
+ * - Event queue: on
+ * - Pin assignment: TxD (default), RxD (default)
+ */
+#define TX1 														27                                                                //
+#define RX1 														26
+#define LED 														13
+#define EX_UART_NUM 												UART_NUM_0
+#define PATTERN_CHR_NUM    											3        /*!< Set the number of consecutive and identical characters received by receiver which defines a UART pattern*/
+#define BUF_SIZE 													1024
+#define RD_BUF_SIZE 												BUF_SIZE
+static QueueHandle_t uart0_queue;
+static QueueHandle_t uart1_queue;
+static QueueHandle_t Cola;
+static QueueHandle_t Cola1;
 
-EventBits_t xEventbits;
+struct TRAMA{
+	uint8_t dato[BUF_SIZE];
+	uint16_t size;
+};
 
-const int BEGIN_TASK1 = BIT0;
-
-const int BEGIN_TASK2 = BIT1;
-
-const int BEGIN_TASK3 = BIT2;
-
-
-
-int a = 0;
-
-uint8_t puerta_abierta = 0;
-
-void Retraso1 (void *P){
+#define SIM800l_PWR_KEY (4)
+#define SIM800l_PWR (23)
+#define SIM800l_RST (5)
 
 
-	printf("Entre en retraso 1 \r\n");
-	for(;;){
-		xEventGroupWaitBits(event_group,BEGIN_TASK1,true,true,portMAX_DELAY);
-		printf("Esperare 4 s \r\n");
-		vTaskDelay(4000 / portTICK_PERIOD_MS);
-		xEventGroupClearBits(event_group, BEGIN_TASK1);
-		printf("Ya espere 4 \r\n");
-		xEventGroupSetBits(event_group, BEGIN_TASK2);
-		printf("Ya espere 4.1 \r\n");
-	}
+static void uart_event_task(void *pvParameters)
+{
+   uart_event_t event;
+   struct TRAMA TX;
 
+    for(;;) {
+
+       if(xQueueReceive(uart0_queue, (void * )&event, (portTickType)portMAX_DELAY)) {
+            bzero(TX.dato, RD_BUF_SIZE);
+
+            switch(event.type) {
+                case UART_DATA:
+
+                    TX.size=(uint16_t)event.size;
+                    uart_read_bytes(EX_UART_NUM, TX.dato, TX.size, portMAX_DELAY);
+                    xQueueSend(Cola,&TX,0/portTICK_RATE_MS);
+                    break;
+                default:
+                    ESP_LOGI(TAG, "uart event type: %d", event.type);
+                    break;
+            }
+        }
+    }
+    vTaskDelete(NULL);
 }
 
-void Retraso2 (void *P){
+static void uart1_event_task(void *pvParameters)
+{
+   uart_event_t event;
+   struct TRAMA TX;
 
-	printf("Entre en retraso 2 \r\n");
-	for(;;){
-		xEventGroupWaitBits(event_group,BEGIN_TASK2,true,true,portMAX_DELAY);
-		printf("Esperare 5 s \r\n");
-		vTaskDelay(5000 / portTICK_PERIOD_MS);
-		xEventGroupClearBits(event_group, BEGIN_TASK2);
-		printf("Ya espere 5.1 \r\n");
-		xEventGroupSetBits(event_group, BEGIN_TASK3);
-		printf("Ya espere 5.2 \r\n");
-	}
+    for(;;) {
 
+       if(xQueueReceive(uart1_queue, (void * )&event, (portTickType)portMAX_DELAY)) {
+            bzero(TX.dato, RD_BUF_SIZE);
+
+            switch(event.type) {
+                case UART_DATA:
+                    TX.size=(uint16_t)event.size;
+                    uart_read_bytes(UART_NUM_1, TX.dato, TX.size, portMAX_DELAY);
+                    xQueueSend(Cola1,&TX,0/portTICK_RATE_MS);
+                    break;
+                default:
+                    ESP_LOGI(TAG, "uart event type: %d", event.type);
+                    break;
+            }
+        }
+    }
+    vTaskDelete(NULL);
 }
 
-void Retraso3 (void *P){
+static void task2(void *pvParameters){
+	struct TRAMA RX;
+	  	  for(;;) {
+	  		xQueueReceive(Cola,&RX,portMAX_DELAY);
+	  		uart_write_bytes(UART_NUM_1, (const char*)RX.dato, RX.size);
 
-	printf("Entre en retraso 3 \r\n");
-	for(;;){
-		xEventGroupWaitBits(event_group,BEGIN_TASK3,pdFALSE,true,portMAX_DELAY);
-		printf("Esperare 6 s \r\n");
-		vTaskDelay(6000 / portTICK_PERIOD_MS);
-		xEventGroupClearBits(event_group, BEGIN_TASK3);
-		printf("Ya espere 6.1 \r\n");
-		xEventGroupSetBits(event_group, BEGIN_TASK1);
-		printf("Ya espere 6.2 \r\n");
-	}
-
+	  	  	  }
+	    vTaskDelete(NULL);
 }
 
+static void task3(void *pvParameters){
+	struct TRAMA RX;
+	  	  for(;;) {
+	  		xQueueReceive(Cola1,&RX,portMAX_DELAY);
+	  		uart_write_bytes(UART_NUM_0, (const char*)RX.dato, RX.size);
 
+	  	  	  }
+	    vTaskDelete(NULL);
+}
 void app_main(void)
 {
 
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
+	Cola= xQueueCreate(1, sizeof(struct TRAMA));
+	Cola1= xQueueCreate(1, sizeof(struct TRAMA));
 
-	a = 2;
-	printf("%d \r\n",a );
-	gpio_pad_select_gpio(GPIO_NUM_19);
-	gpio_set_direction(GPIO_NUM_19, GPIO_MODE_INPUT);
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
 
-	 event_group = xEventGroupCreate();
+    };
 
+    uart_driver_install(UART_NUM_1, BUF_SIZE, BUF_SIZE, 20, &uart1_queue, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    //Install UART driver, and get the queue.
+    uart_driver_install(EX_UART_NUM, BUF_SIZE, BUF_SIZE, 20, &uart0_queue, 0);
+    uart_param_config(EX_UART_NUM, &uart_config);
 
-	 xTaskCreatePinnedToCore(&Retraso1, "Retraso1", 1024, NULL, 8, NULL,0);
-	 xTaskCreatePinnedToCore(&Retraso2, "Retraso2", 1024, NULL, 6, NULL,0);
-	 xTaskCreatePinnedToCore(&Retraso3, "Retraso3", 1024, NULL, 4, NULL,0);
-
-	 printf("%d \r\n",a );
-	 xEventGroupSetBits(event_group, BEGIN_TASK1);
-
-	 while(1){
-
-		if (gpio_get_level(GPIO_NUM_19) == 1){
-			printf("entre al if\r\n");
-			puerta_abierta = 1;
-
-			xEventbits = xEventGroupGetBits( event_group );
-			if( xEventbits != BEGIN_TASK3 )
-				{
-				printf("NO esta en el retraso 3 \r\n");
-				}
-			if( xEventbits == BEGIN_TASK3 )
-				{
-				printf("SI esta en el retraso 3 \r\n");
-			}
+    //Install UART driver, and get the queue.
 
 
-			vTaskDelay(200 / portTICK_PERIOD_MS);
-		}
+    //Set UART log level
+    esp_log_level_set(TAG, ESP_LOG_INFO);
+    //Set UART pins (using UART0 default pins ie no changes.)
+    uart_set_pin(EX_UART_NUM, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    //Set UART pins (using UART1 default pins ie no changes.)
+    uart_set_pin(UART_NUM_1, TX1, RX1, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    //Set uart pattern detect function.
+   // uart_enable_pattern_det_baud_intr(EX_UART_NUM, '+', PATTERN_CHR_NUM, 9, 0, 0);
+    //Reset the pattern queue length to record at most 20 pattern positions.
+  //  uart_pattern_queue_reset(EX_UART_NUM, 20);
+
+    //Create a task to handler UART event from ISR
+   // xTaskCreate(task_test, "tarea de prueba", 3*1024, NULL, 2, &xTask2Handle);
+    xTaskCreate(task2, "tarea de prueba", 4*1024, NULL, 2, NULL);
+    xTaskCreate(task3, "tarea 3 de prueba", 4*1024, NULL, 2, NULL);
+    xTaskCreate(uart_event_task, "uart_event_task", 10*2048, NULL, 1, NULL);
+    xTaskCreate(uart1_event_task, "uart1_event_task", 10*2048, NULL, 1, NULL);
+
+    /*Configurar inicio del SIM800l*/
+    	/*Poner los pines como GPIO*/
+    	gpio_pad_select_gpio(SIM800l_PWR_KEY);
+    	gpio_pad_select_gpio(SIM800l_PWR);
+    	gpio_pad_select_gpio(SIM800l_RST);
+    	/*Configurar los GPIO como output, el RST como Open Drain por seguridad*/
+    	gpio_set_direction(SIM800l_PWR_KEY, GPIO_MODE_OUTPUT);
+    	gpio_set_direction(SIM800l_PWR, GPIO_MODE_OUTPUT);
+    	gpio_set_direction(SIM800l_RST, GPIO_MODE_OUTPUT_OD);
+
+        gpio_set_level(SIM800l_PWR, 1);
+        gpio_set_level(SIM800l_RST, 1);
+        gpio_set_level(SIM800l_PWR_KEY, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(SIM800l_PWR_KEY, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(SIM800l_PWR_KEY, 1);
 
 
-		vTaskDelay(200 / portTICK_PERIOD_MS);
-	}
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        ESP_LOGW("HOLA","Ya espere 5");
 
+        // Se activan las funcionalidades
+        ESP_LOGW("HOLA","Mando CFUN");
+        uart_write_bytes(UART_NUM_1,"AT+CFUN=1\r\n", 11);
+        ESP_LOGW(TAG, "CFUN activo \r\n");
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
 
+        //Para conectarse a la red de Movistar
+        ESP_LOGW("HOLA","Mando CSTT");
+        uart_write_bytes(UART_NUM_1,"AT+CSTT=\"internet.movistar.ve\",\"\",\"\"", 43);
+        ESP_LOGW(TAG, "Conectandose a movistar \r\n");
 
-//    gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-//    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-//    while(1) {
-        /* Blink off (output low) */
-//	printf("Turning off the LED\n");
-//        gpio_set_level(BLINK_GPIO, 0);
-//        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-//	printf("Turning on the LED\n");
-//        gpio_set_level(BLINK_GPIO, 1);
-//        vTaskDelay(1000 / portTICK_PERIOD_MS);
- //   }
+        vTaskDelay(40000 / portTICK_PERIOD_MS);
 
+        // Para activar la conexion inalambrica por GPRS
+	    ESP_LOGW(TAG, "Mando Ciirc\r\n");
+		uart_write_bytes(UART_NUM_1,"AT+CIICR\r\n", 10);
+	    ESP_LOGW(TAG, "Ciirc activando \r\n");
 
 }
+
+
